2661c62c41b9d2230da0dd0e0a1a23a5fd7874ab
Terra-exe <102436774+Terra-exe@users.noreply.github.com>
2024-09-26 19:50:49 -0700

    j

---
+++ rust-wasm/target/debug/deps/unicode_ident-9bc4408ccb95ab77.d
@@ -0,0 +1,6 @@
+D:\Glaum Consciousness\rust-wasm\target\debug\deps\libunicode_ident-9bc4408ccb95ab77.rmeta: C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.13\src/lib.rs C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.13\src\tables.rs
+
+D:\Glaum Consciousness\rust-wasm\target\debug\deps\unicode_ident-9bc4408ccb95ab77.d: C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.13\src/lib.rs C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.13\src\tables.rs
+
+C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.13\src/lib.rs:
+C:\Users\Admin\.cargo\registry\src\index.crates.io-6f17d22bba15001f\unicode-ident-1.0.13\src\tables.rs:
