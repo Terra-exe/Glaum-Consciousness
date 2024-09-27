4af58bbf4899e15c65dbd016ce2d10c8f5814358
Terra-exe <102436774+Terra-exe@users.noreply.github.com>
2024-09-26 18:06:04 -0700

    sfd

---
+++ rust-wasm/target/debug/deps/rust_wasm-149dafec1896166f.d
@@ -0,0 +1,5 @@
+D:\Glaum Consciousness\rust-wasm\target\debug\deps\librust_wasm-149dafec1896166f.rmeta: src\lib.rs
+
+D:\Glaum Consciousness\rust-wasm\target\debug\deps\rust_wasm-149dafec1896166f.d: src\lib.rs
+
+src\lib.rs:
